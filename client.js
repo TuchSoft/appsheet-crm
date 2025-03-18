caba26aad40e920dd2e8739ee2a7fb79c86f97c6
Mattia Bonzi <mattia@mattiabonzi.it>
2025-03-18 01:14:15 +0100

    uodt

--- client.js
+++ client.js
@@ -197,6 +197,7 @@ function addGeneralStyle() {
                 .SlideshowPage__heading-label {font-weight: 600; font-size: 1rem;}
                 .BaseTypeDisplay {font-size: 0.95rem;}
                 .BasePrimaryView:first-child { background-color:rgb(195, 196, 198);}
+                .slideshow-page-header div .TextTypeDisplay__text:first-child { font-size: 1.5rem; font-weight: 600; }
                 
            `);
 }
